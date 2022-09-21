49197dedd563f1e70af56dfb5c35dd2c0940b50b
eritol4047 <112415918+eritol4047@users.noreply.github.com>
2022-09-21 08:41:15 -0400

    Add files via upload
    First Upload of Group Project CIS334 and CIS334L

---
+++ Project_Strummer_Boy 1.2.0/PSB_JS.js
@@ -0,0 +1,93 @@
+var go = document.getElementById('lowere1'), audio = null;
+
+//------------------------------First Fret---------------------------->
+function lowerE1() { //Lower E1 function
+    audio = new Audio("./sounds/Lower-E1.mp3"); 
+    audio.play();
+}
+
+function a1() { //A1 Function
+    audio = new Audio("./sounds/A1.mp3");
+    audio.play();
+}
+
+function d1() { //D1 Function
+    audio = new Audio("./sounds/D1.mp3");
+    audio.play();
+}
+
+function g1() { //G1 Function
+    audio = new Audio("./sounds/G1.mp3");
+    audio.play();
+}
+
+function b1() { //B1 Fucntion
+    audio = new Audio("./sounds/B1.mp3");
+    audio.play();
+}
+
+function upperE1() { //Upper E1 fucntion
+    audio = new Audio("./sounds/Upper-E1.mp3");
+    audio.play();
+}
+
+//-----------------------------Second Fret--------------------------------->
+function lowerE2() { //Lower E2 function
+    audio = new Audio("./sounds/Lower-E2.mp3");
+    audio.play();
+}
+
+function a2() { //A2 function
+    audio = new Audio("./sounds/A2.mp3");
+    audio.play();
+}
+
+function d2() { //D2 function
+    audio = new Audio("./sounds/D2.mp3");
+    audio.play();
+}
+
+function g2() { //G2 function
+    audio = new Audio("./sounds/G2.mp3");
+    audio.play();
+}
+
+function b2() { //B2 function
+    audio = new Audio("./sounds/B2.mp3");
+    audio.play();
+}
+
+function upperE2() { //Upper E2 function
+    audio = new Audio("./sounds/Upper-E2.mp3");
+    audio.play();
+}
+//-----------------------------Third Fret---------------------------------->
+function lowerE3() { //Lower E3 function
+    audio = new Audio("./sounds/Lower-E3.mp3");
+    audio.play();
+}
+
+function a3() { //A3 function
+    audio = new Audio("./sounds/A3.mp3");
+    audio.play();
+}
+
+function d3() { //D3 function
+    audio = new Audio("./sounds/D3.mp3");
+    audio.play();
+}
+
+function g3() { //G3 function
+    audio = new Audio("./sounds/G3.mp3");
+    audio.play();
+}
+
+function b3() { //B3 function
+    audio = new Audio("./sounds/B3.mp3");
+    audio.play();
+}
+
+function upperE3() { //Upper E3 function
+    audio = new Audio("./sounds/Upper-E3.mp3");
+    audio.play();
+}
